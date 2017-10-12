903bcf394d5cebb87b77d43d3c92f6fbc0ca9b8f
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-12 08:59:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+
 end
